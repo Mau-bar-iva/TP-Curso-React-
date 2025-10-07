fa7976b26510e281951da6771cc7b553331048ae
Mauricio <zub_gg@hotmail.com>
2025-10-07 02:53:04 -0300

    images urls update and carrito link update

--- src/components/Header/nav/Navbar.jsx
+++ src/components/Header/nav/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 
-import { Link } from 'react-router-dom';
+import { HashRouter as Link } from 'react-router-dom';
 import './Navbar.css';
 import { CarritoContext } from '../../Main/Carrito/CarritoContextDef';
 
